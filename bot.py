1ab415b7c8b75ef23506634964b6adb30062639a
Dhruman Bhadeshiya <dhruman@kevit.io>
2022-02-14 16:44:20 +0530

    improved bot response messages

--- bot.py
+++ bot.py
@@ -7,17 +7,18 @@ from helper import detect_entity, detect_intent, get_definition, generate_thumbn
 class EntityDetectionBot(ActivityHandler):
 
     async def on_message_activity(self, turn_context: TurnContext):
+        message = "Sorry I am unable to resolve your query at the moment. For more info, you can contact us on " \
+                  "support@johnsnowlabs.com or call us at +1-302-786-5227."
         detected_intent = detect_intent(turn_context.activity.text)
-        if detected_intent:
-            message = "Detected Intent: " + detected_intent
+        if detected_intent and detected_intent != "None":
+            message = "Detected Intent is: " + detected_intent
             await turn_context.send_activity(message)
-        detected_entity = detect_entity(turn_context.activity.text)
-        if detected_entity:
-            definition, matched_word = get_definition(detected_entity)
-            message = generate_thumbnail_card(detected_entity, definition, matched_word)
-        else:
-            message = "Sorry! I was unable to understand."
-
+            detected_entity = detect_entity(turn_context.activity.text)
+            if detected_entity:
+                message = "First entity which is extracted is: " + detected_entity
+                definition, matched_word = get_definition(detected_entity)
+                await turn_context.send_activity(message)
+                message = generate_thumbnail_card(detected_entity, definition, matched_word)
         await turn_context.send_activity(message)
 
     async def on_members_added_activity(
@@ -27,4 +28,5 @@ class EntityDetectionBot(ActivityHandler):
     ):
         for member_added in members_added:
             if member_added.id != turn_context.activity.recipient.id:
-                await turn_context.send_activity("Hello and welcome!")
+                await turn_context.send_activity("Hello!")
+                await turn_context.send_activity("How may I help you?")
